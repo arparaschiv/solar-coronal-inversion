9d612bbcfb6fd3d3664556e3771a8a71776533b5
arparaschiv <paraschiv.alinrazvan+git@gmail.com>
2023-01-31 00:22:21 -0700

    update-readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,11 +26,12 @@ release = 'update-readthedocs'
 
 
 # -- General configuration ---------------------------------------------------
-#import sphinx_rtd_theme ##PAR import as in https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html
+import sphinx_rtd_theme ##PAR import as in https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html
+import myst_parser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx_rtd_theme"]
+extensions = ["myst_parser","sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
