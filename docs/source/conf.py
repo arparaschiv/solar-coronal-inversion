685dbfcadd0b7b8b7b06c3d6c2f2535528d77593
arparaschiv <paraschiv.alinrazvan+git@gmail.com>
2023-01-31 15:23:21 -0700

    update-readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ release = 'update-readthedocs'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx_rtd_theme"]
-
+#extensions = ["myst_parser", "sphinx_rtd_theme"]
+extensions =[]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -50,8 +50,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
+#html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
