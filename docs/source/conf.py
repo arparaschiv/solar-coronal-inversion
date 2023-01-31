01444e4357bb7374d40ad23c9134050fedc613f3
arparaschiv <paraschiv.alinrazvan+git@gmail.com>
2023-01-30 23:26:29 -0700

    update-readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ import myst_parser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser","sphinx_rtd_theme"]
+extensions = ["myst-parser","sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
